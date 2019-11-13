9f6b6fa34bfc114326c828e227f479cb63cf973a
collins kirwa <collinskirwack@gmail.com>
2019-11-13 15:26:30 +0300

    Called with explicit and declared three constants

--- 2-functions/functions.js
+++ 2-functions/functions.js
@@ -98,7 +98,9 @@ describe("functions", function() {
             // and 'answerThirdResult' with the correct results.
             // If you call getContext() here, you're a cheater! :)
             // ✏️ ADD CODE HERE ✏️
-
+                const answerFirstResult = getContext()
+                const answerSecondResult = getContext()
+                const answerThirdResult = getContext()
             ////////////////////////////////////////////////////////////////////
 
             // First we call the function without an explicit context
