025765be926f53608a865bcd381f1fc1016ea030
collins kirwa <collinskirwack@gmail.com>
2019-11-19 10:09:08 +0300

    Debbugged Lost Context

--- 2-functions/functions.js
+++ 2-functions/functions.js
@@ -629,7 +629,7 @@ const context = {};
 
             // Now we're registering the service.handleEvent method so that
             // it will be called as soon as the event is emitted.
-            eventEmitter.addEventListener(service.handleEvent);
+            eventEmitter.addEventListener(service.handleEvent.bind(service));
             // Now let's emit the event. However, this will fail.
             // Find the reason why and fix it.
             eventEmitter.emit();
